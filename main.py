a152afe6cc16a9ce42797404b78c5b9118045cec
Elian Kurtenkov <elian90@abv.bg>
2017-02-27 00:27:20 +0200

    Read img, filter white figures

--- main.py
+++ main.py
@@ -9,25 +9,33 @@ from array import array
 import numpy as np
 
 def processImg(img):
-    cv2.imshow('Main', img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    b1 = np.array([240,240,240])
+    b2 = np.array([255,255,255])
+    img2 = cv2.inRange(hsv, b1, b2);
+    
+    cv2.imshow('Main', img2)
     return
 
 def main():    
     cv2.startWindowThread()
     cv2.namedWindow("Main", cv2.WINDOW_AUTOSIZE)     
-    hwnd = win32gui.FindWindow(None, "Play Stick Fight game online - Y8.COM")
+    hwnd = win32gui.FindWindow("Qt5QWindowIcon", "KOPLAYER 1.4.1055")
+    rect = win32gui.GetClientRect(hwnd)
     wDC = win32gui.GetWindowDC(hwnd)
     dcObj=win32ui.CreateDCFromHandle(wDC)
     cDC=dcObj.CreateCompatibleDC()
     dataBitMap = win32ui.CreateBitmap()
-    dataBitMap.CreateCompatibleBitmap(dcObj, 1920, 1080)
+    width = rect[2]
+    height = rect[3]
+    dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
     cDC.SelectObject(dataBitMap)
         
     while(True):
-        cDC.BitBlt((0,0),(1920, 1080) , dcObj, (0,0), win32con.SRCCOPY)
+        cDC.BitBlt((0,0),(width, height) , dcObj, (0,0), win32con.SRCCOPY)
         signedIntsArray = dataBitMap.GetBitmapBits(True)
         img = np.fromstring(signedIntsArray, dtype='uint8')
-        img.shape = (1080,1920,4)
+        img.shape = (height, width, 4)
         processImg(img)       
         cv2.waitKey(1) 
             
